d92e2777e86781e4aca2a0656989413be7037e8a
Jericho Rivera <jerichorivera@gmail.com>
2024-09-12 21:13:48 +0800

    Add firewall rules block

---
+++ gcp/firewall/firewall.tf
@@ -0,0 +1,16 @@
+resource "google_compute_firewall" "firewall" {
+  name    = var.firewall_name
+  network = data.google_compute_network.default.name
+
+  dynamic "allow" {
+    for_each = var.set_allow_list ? var.allow_list : []
+
+    content {
+      protocol = lookup(allow.value, "protocol", null)
+      ports    = lookup(allow.value, "ports", null)
+    }
+  }
+
+  source_tags = var.source_tags
+  target_tags = var.target_tags
+}
